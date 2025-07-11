a75ece8463a4678d3240f92bfd5e31410b5e2072
soravsingh08 <rahulrakwal3@gmail.com>
2025-07-11 19:40:27 +0530

    Initial commit: React + Vite

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import padsData from "./pads"
+
+export default function App() {
+    const [pads, setPads] = React.useState(padsData)
+    
+    const buttonElements = pads.map(pad => (
+        <button key={pad.id}></button>
+    ))
+    
+    return (
+        <main>
+            <div className="pad-container">
+                {buttonElements}
+            </div>
+        </main>
+    )
+}
